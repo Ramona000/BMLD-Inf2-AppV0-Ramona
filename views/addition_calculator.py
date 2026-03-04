f32fcd1c6aff878f04c3a434fdfeacda44d4139f
SamiraHofer <samirahofer@hispeed.ch>
2026-03-04 10:36:31 +0100

    Bis und mit Konzentration =

--- views/addition_calculator.py
+++ views/addition_calculator.py
@@ -1,8 +1,16 @@
 import streamlit as st
+import math
 from functions.addition import add, subtract
-st.title("Addition")
+st.title("pH-Rechner")
 
-st.write("Hier ist mein Rechner.")
+st.write("Berechne deine pH-Werte.")
+
+typ= st.selectbox("Wähle den Lösungstyp", ["Säure", "Base"])
+
+konzentration = st.number_input(
+    "Konzentration (mol/L)",
+    min_value=0.0,
+    format="%.6f")
 
 with st.form("addition_form"):
     nummer_1 = st.number_input("erste Zahl")
